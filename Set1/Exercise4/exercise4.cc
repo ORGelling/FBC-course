226aa2c765582843c8f8e5cb4bd06515061d8d4d
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-03 23:31:34 +0200

    Exercise 4 comments and info txt file added

--- Set1/Exercise4/exercise4.cc
+++ Set1/Exercise4/exercise4.cc
@@ -2,11 +2,12 @@
 
 using namespace std;
 
+// We use R"special( and )special" to denote the start and end of string
 string rawLiteral = R"special(^\s+Encryption key:(\w+)
 ^\s+Quality=(\d+)
 ^\s+E?SSID:"([[:print:]]+)"
 ^\s+ssid="([[:print:]]+)"
-)special";
+)special";                          // adding the newline by placing this here
 
 
 int main()
