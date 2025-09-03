ab34fe9ccdfe6c58230fd76b7215f19b444d2fa0
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-03 18:51:00 +0200

    Exercise 4 with output file

--- Set1/Exercise4/exercise4.cc
+++ Set1/Exercise4/exercise4.cc
@@ -2,4 +2,14 @@
 
 using namespace std;
 
+string rawLiteral = R"special(^\s+Encryption key:(\w+)
+^\s+Quality=(\d+)
+^\s+E?SSID:"([[:print:]]+)"
+^\s+ssid="([[:print:]]+)"
+)special";
 
+
+int main()
+{
+    cout << rawLiteral;
+}
