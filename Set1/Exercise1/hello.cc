65bebabd45dc1a58e3e6ac390fa179dba82f396a
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-03 14:46:00 +0200

    directory per exercise and improved hello world

---
+++ Set1/Exercise1/hello.cc
@@ -0,0 +1,8 @@
+#include <iostream>                     // importing in-out-stream
+
+using namespace std;                    // adding namespace
+
+int main()                              // args not needed explicitly
+{
+    cout << "Hello World\n";
+}
