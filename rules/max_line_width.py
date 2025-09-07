36bb545648857268030d7050ba9e27436e901c9f
Leon Lan <l.lan@vu.nl>
2025-09-07 15:27:46 +0200

    Refactor

--- rules/max_line_width.py
+++ rules/max_line_width.py
@@ -2,9 +2,10 @@
 import sys
 
 max_width = int(sys.argv[1])
+
 for filename in sys.argv[2:]:
-    with open(filename, "r") as f:
-        for i, line in enumerate(f, 1):
+    with open(filename, "r") as fh:
+        for idx, line in enumerate(fh, 1):
             if len(line.rstrip()) > max_width:
-                print(f"{filename}:{i}: line too long")
+                print(f"{filename}:{idx}: line too long")
                 sys.exit(1)
