65775fc6cf6d49da9882e09b4b7b065e90d1dc84
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-07 15:40:47 +0200

    Adding Leon's corrections!

--- Set1/04/exercise4.cc
+++ Set1/04/exercise4.cc
@@ -2,15 +2,19 @@
 
 using namespace std;
 
+namespace {                         // anonymous namespace
+
 // We use R"special( and )special" to denote the start and end of string
-string rawLiteral = R"special(^\s+Encryption key:(\w+)
+const char wifiPattern[] = 
+R"special(^\s+Encryption key:(\w+)
 ^\s+Quality=(\d+)
 ^\s+E?SSID:"([[:print:]]+)"
 ^\s+ssid="([[:print:]]+)"
 )special";                          // adding the newline by placing this here
 
+};                                  // end of namespace
 
 int main()
 {
-    cout << rawLiteral;
+    cout << wifiPattern;
 }
