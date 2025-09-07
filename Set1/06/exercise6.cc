e8afa4d695f8416d78d03f881b5d8bbdf97a1c73
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-07 18:34:14 +0200

    added blanks and moved comments after code as per abbreviations

--- Set1/06/exercise6.cc
+++ Set1/06/exercise6.cc
@@ -7,16 +7,21 @@ int main()
     size_t value;
     cin >> value;
 
-    // Simple modulo 2. 0 should give false and any nonzero value true
     cout << (value % 2 ? "odd" : "even") << '\n';
-    // bitwise AND to select the LSB. 1/true for odd, 0/false for even
+    // Simple modulo 2. 0 gives false, nonzero true
+
     cout << (value & 1 ? "odd" : "even") << '\n';
-    // bitshift right and then left to see if the LSB goes from 1 to 0.
+    // bitwise AND to select the LSB. 1/true for odd, 0/false for even
+
     cout << ((value >> 1) << 1 != value ? "odd" : "even") << '\n';
-    // divide and multiply by 2 to see if uneven numbers lose their .5
+    // bitshift right and left to see if LSB goes from 1 to 0.
+
     cout << ((value / 2) * 2 != value ? "odd" : "even") << '\n';
-    // use bitwise OR to check if the last bit flips or not
+    // divide and multiply by 2 to see if uneven numbers lose their .5
+
     cout << ((value | 1) == value ? "odd" : "even") << '\n';
-    // use bitwise XOR to see if the last bit flips up or down
+    // use bitwise OR to check if last bit flips
+
     cout << ((value ^ 1) + 1 == value ? "odd" : "even") << '\n';
+    // use bitwise XOR to see if the last bit flips up or down
 }
