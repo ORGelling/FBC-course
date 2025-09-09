9d6a8c147792a435e0ad6f07bf1f4803cf32e590
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-09 12:45:48 +0200

    Added Efe's feedback to our set1 answers

---
+++ Set1Feedback/o1757339008.Set1/04/exercise4.cc
@@ -0,0 +1,20 @@
+// ES: 1
+#include <iostream>
+
+using namespace std;
+
+namespace {                         // anonymous namespace
+
+char const wifiPattern[] =
+R"special(^\s+Encryption key:(\w+)
+^\s+Quality=(\d+)
+^\s+E?SSID:"([[:print:]]+)"
+^\s+ssid="([[:print:]]+)"
+)special";                          // adding newline by placing this here
+// We use R"special( and )special" to denote the start and end of the string
+};                                  // end of namespace
+
+int main()
+{
+    cout << wifiPattern;
+}
