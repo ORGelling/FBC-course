b134e50b32938b9c3662f1018bbc71292b90f688
Leon Lan <l.lan@vu.nl>
2025-09-10 16:47:45 +0200

    Fix

--- Set2/09/tmp.cc
+++ Set2/09/tmp.cc
@@ -2,23 +2,23 @@
 #include <string>
 
 int main() {
-    size_t countC = 0;
     size_t countA = 0;
+    size_t countE = 0;
     size_t countI = 0;
 
     std::string line;
     while (std::getline(std::cin, line)) {
-        std::cout << line;
+        std::cout << line << '\n'; // debug
         for (auto const &c : line) {
             if (c == 'a' || c == 'A')
-                ++countC;
-            else if (c == 'c' || c == 'C')
                 ++countA;
+            else if (c == 'e' || c == 'E')
+                ++countE;
             else if (c == 'i' || c == 'I')
                 ++countI;
         }
     }
 
-    std::cout << "# a-chars: " << countA << ", " << "# e-chars: " << countC
+    std::cout << "# a-chars: " << countA << ", " << "# e-chars: " << countE
               << ", " << "# i-chars: " << countI << '\n';
 }
