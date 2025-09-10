71435247971a6951b0f9f46e67d4aeae3fc463e6
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-10 18:18:18 +0200

    Exercise 10's finalised code, including info and order file

--- Set2/10/exercise10.cc
+++ Set2/10/exercise10.cc
@@ -2,3 +2,19 @@
 #include <string>
 
 using namespace std;
+
+int main()
+{
+    cout << "? ";                   // Prompt to ask for input
+    string line;                    // Declaring string
+    getline(cin, line);             // Getting input line
+    
+    cout << '`' 
+        << line.substr              // Extract relevant part of string
+            (                       // starting when blanks end
+                line.find_first_not_of(" "),
+                line.find_last_not_of(" ") -
+                    line.find_first_not_of(" ") + 1
+            )                       // and ending when only blanks left
+        << '\'' << '\n';            // +1 for index offset
+}
