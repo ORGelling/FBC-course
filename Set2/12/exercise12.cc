53ed6545cf7c00d7e4534b2175e9d7c87630b053
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-11 22:13:26 +0200

    Updated ex 12 and its info. Should look a lot cleaner now

--- Set2/12/exercise12.cc
+++ Set2/12/exercise12.cc
@@ -5,22 +5,20 @@ using namespace std;
 
 int main()
 {
-    string input;                       // We use strings here to easily
-    cin >> input;                       // cut our input value up
+    string input;                       // Handle input
+    cin >> input;
     
-    size_t sum;                         // We need sum to persist
-    do                                  // Loop sum until 1 digit left
+    while (input.length() > 1)          // Loop sum until 1 digit left
     {
-        sum = 0;                        // Set sum to 0 every loop
+        size_t sum = 0;                 // Reset sum every loop
         for (char& c : input)           // Loop through input per character
-            sum += c - '0';             // Sum current character as int
-        input = to_string(sum);         // Use input string to store sum
+            sum += c - '0';             // Sum value of current character
+        input = to_string(sum);         // Use 'input' to store 'sum'
     }
-    while (input.length() > 1);         // While input has multiple digits
     
     cout << "\""
-        << (string{3, 6, 9}.find(sum)           // Find 3, 6, or 9 
-                == string::npos                 // npos for not found
+        << (string{3, 6, 9}.find(input[0] - '0')    // Find 3, 6, or 9 
+                == string::npos                     // npos for not found
                 ? "not " : "")
         <<  "divisible by 3" << '\n';       // Return output
 }
