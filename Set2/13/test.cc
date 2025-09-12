0a95860fa040405b4ef8cb1d42d53a6fb020fc54
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-12 13:44:27 +0200

    finished ex 13, added order and example files

---
+++ Set2/13/test.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int value = 8;
+    
+    //if (true) if (false) value = 5; else value = 6;
+    
+    //if (true) 
+    //    if (false) value = 5;             // We can test which if gets us
+    //else value = 6;                       // to the else here
+    
+    //if (true)                             // This one has no else now
+    //    if (false)                        // This leads to the else
+    //        value = 5; 
+    //    else value = 6;
+    
+    //if (false)                            // Now this one does
+    //{
+    //    if (false)                        // This one does not 
+    //        value = 5; 
+    //}
+    //else 
+    //    value = 6;
+    
+    if (true)
+    {
+        if (false)                          // Now this one leads to the else
+            value = 5; 
+        else value = 6;
+    }
+    
+    cout << value << '\n';
+}
