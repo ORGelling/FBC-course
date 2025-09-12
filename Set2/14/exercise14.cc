0e5009a22dacf17029e1408d4bc0bc7ac8c2929e
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-12 16:11:55 +0200

    Finished exercise 14, info, and order files

---
+++ Set2/14/exercise14.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    while (true)                        // Repeat until exit command given
+    {
+        cout << "Enter a value to comupte the sqrt of (<= 0 to stop): ";
+        double input;                   // Prompt, initialise,
+        cin >> input;                   // and receive input
+        
+        if (input <= 0) break;          // Program's exit condition
+        
+        double xZero = 1;               // Initialising calculation args
+        double xOne;
+        
+        size_t count = 1;               // Starting our loop count
+        while (true)                    // Loop algorithm until broken
+        {
+            xOne = xZero + (input - (xZero * xZero)) / (2 * xZero);
+                                            // Execute algorithm
+            if (xZero == xOne) break;   // Break when values align
+            
+            cout << count << ": x0: " << xZero
+                << ", x1 = " << xOne
+                << ", sqr is: " << xOne * xOne
+                << '\n';                // Output intermediate steps
+            
+            xZero = xOne;               // Reassign x0
+            ++count;                    // increment count
+        }
+        
+        cout << "sqrt(" << input << ") is " << xZero << '\n';
+    }                                   // Show result when done
+}
