717b6d5db3b83f30e1fc620f434f1e25702a51b9
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-12 18:45:32 +0200

    improvements ex 14

--- Set2/14/exercise14.cc
+++ Set2/14/exercise14.cc
@@ -4,31 +4,30 @@ using namespace std;
 
 int main()
 {
-    while (true)                        // Repeat until exit command given
+    while (true)                            // Repeat until exit command
     {
         cout << "Enter a value to comupte the sqrt of (<= 0 to stop): ";
-        double input;                   // Prompt, initialise,
-        cin >> input;                   // and receive input
+        double input;                       // Prompt, initialise,
+        cin >> input;                       // and receive input
         
-        if (input <= 0) break;          // Program's exit condition
+        if (input <= 0) break;              // Program's exit condition
         
-        double xZero = 1;               // Initialising calculation args
-        double xOne;
+        double xZero = 1;                   // Initialising calculation args
         
-        size_t count = 1;               // Starting our loop count
-        while (true)                    // Loop algorithm until broken
-        {
-            xOne = xZero + (input - (xZero * xZero)) / (2 * xZero);
-                                            // Execute algorithm
-            if (xZero == xOne) break;   // Break when values align
+        size_t count = 1;                   // Starting our loop count
+        while (true)
+        {                                   // Executing algorithm
+            double xOne = xZero + (input - (xZero * xZero)) / (2 * xZero);
+                                                // Minimal xOne scope
+            if (xZero == xOne) break;       // Break when values align
             
             cout << count << ": x0: " << xZero
                 << ", x1 = " << xOne
                 << ", sqr is: " << xOne * xOne
-                << '\n';                // Output intermediate steps
-            
-            xZero = xOne;               // Reassign x0
-            ++count;                    // increment count
+                << '\n';                    // Output this step
+        
+            xZero = xOne;                       // Reassign x0
+            ++count;                            // increment count
         }
         
         cout << "sqrt(" << input << ") is " << xZero << '\n';
