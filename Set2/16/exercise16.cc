f31330cc821db7a9ed0f964cbcba2cfa3edde36e
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-13 16:41:56 +0200

    exercise 16 with info and order files complete

--- Set2/16/exercise16.cc
+++ Set2/16/exercise16.cc
@@ -5,5 +5,18 @@ using namespace std;
 
 int main()
 {
-    
+    for (string line; getline(cin, line);)      // Read line from input
+    {                                           // For in case of file
+        for (size_t index = line.size(); index > 0; --index)
+        {                                       // Loop over all characters 
+            size_t bytes = 1;                   // # bytes read
+            while ((line[index - 1] & 192) == 128)  // Check if MSB is set
+            {                                       // but next one is not
+                --index;                        // Move to next byte
+                ++bytes;                        // Store extra byte #
+            }
+            cout << line.substr(index - 1, bytes);  // Show character without
+        }                                           // truncating bytes
+    cout << '\n';
+    }
 }
