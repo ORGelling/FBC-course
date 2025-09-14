58294b661ebf39dd615dcd6168d0868563834da8
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-14 12:44:05 +0200

    added \f and \v for formfeed and vertical tabs to skipped characters

--- Set2/10/exercise10.cc
+++ Set2/10/exercise10.cc
@@ -12,9 +12,9 @@ int main()
     cout << '`' 
         << line.substr              // Extract relevant part of string
             (                       // starting when blanks end
-                line.find_first_not_of(" \t\n\r"),
-                line.find_last_not_of(" \t\n\r") -
-                    line.find_first_not_of(" \t\n\r") + 1
+                line.find_first_not_of(" \t\n\r\f\v"),
+                line.find_last_not_of(" \t\n\r\f\v") -
+                    line.find_first_not_of(" \t\n\r\f\v") + 1
             )                       // and ending when only blanks left
         << '\'' << '\n';            // +1 for index offset
 }
