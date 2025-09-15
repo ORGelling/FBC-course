512eb92cf7bd035671750cdc95668416934eed6e
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-15 19:30:27 +0200

    improved 15

--- Set2/15/exercise15.cc
+++ Set2/15/exercise15.cc
@@ -5,16 +5,18 @@ using namespace std;
 int main(int argc, char const* argv[])
 {
     ++argv;                                 // Skip the program name
-    size_t const count = --argc;            // Store # of input arguments
-    size_t const total = (1 << count);      // Find # of permutations
+    size_t const nrArgs = --argc;           // Store # of input arguments
+    size_t const nrPerms = (1 << count);    // Find # of permutations
     
     for (size_t argSet = 0; argSet < total; ++argSet)
     {                                       // Output permutation number
         cout << argSet + 1 << ":";
                                             // Cycle through input args
         for (size_t argIdx = 0; argIdx < count; ++argIdx)
-            if (argSet & (1 << argIdx)) cout << " " << argv[argIdx];
-                                            // We filter which args to show
+        {
+            if (argSet & (1 << argIdx)) 
+                cout << " " << argv[argIdx];
+        }                                   // We filter which args to show
         cout << '\n';                       // using a bitshifted mask
     }
 }
