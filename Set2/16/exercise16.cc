9f1fab03b06c4c5afe6e32390684326b8ddf8921
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-15 23:44:18 +0200

    improved 16

--- Set2/16/exercise16.cc
+++ Set2/16/exercise16.cc
@@ -3,8 +3,12 @@
 
 using namespace std;
 
-constexpr unsigned char UTF8_CONT_SEQ = 0b11000000;        // 192
-constexpr unsigned char UTF8_END_SEQ = 0b10000000;         // 128
+namespace {
+    enum { 
+        UTF8_CONT_SEQ = 0b11000000,     // MSB+1, or 192
+        UTF8_END_SEQ = 0b10000000       // Only MSB, or 128
+    };
+}
 
 int main()
 {
@@ -13,7 +17,7 @@ int main()
     {                                           // case of file
         size_t chIndex = line.size(); 
         while(chIndex)
-        {                                       // Loop over all characters 
+        {
             size_t nrBytes = 1;
             while ((line[chIndex - 1] & UTF8_CONT_SEQ) == UTF8_END_SEQ)  
             {                       // Check if MSB is set but next is not
