95755500450cf87cdcf09d9ca48c8433a4455667
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-15 11:26:29 +0200

    improved formatting

--- Set2/13/test.cc
+++ Set2/13/test.cc
@@ -9,9 +9,11 @@ int main()
     //if (true) if (false) value = 5; else value = 6;
     
     //if (true) 
-    //    if (false) value = 5;             // We can test which if gets us
-    //else value = 6;                       // to the else here
-    
+    //    if (false)                        // We can test which if gets us
+    //        value = 5;
+    //else                                  // to the else here
+    //    value = 6;
+
     //if (true)                             // This one has no else now
     //    if (false)                        // This leads to the else
     //        value = 5; 
@@ -28,8 +30,10 @@ int main()
     
     if (true)
     {
-        if (false) value = 5;               // Now this one leads to the else 
-        else value = 6;                     // without warnings
+        if (false)                          // Now this one leads to the else 
+            value = 5;
+        else                                // without warnings
+            value = 6;
     }
     
     cout << value << '\n';
