ada43832641545773c8d472edcd2a84470df2b3a
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-22 00:51:36 +0200

    Update Set3/20/countLines.cc
    Co-authored-by: Leon Lan <30997278+leonlan@users.noreply.github.com>

--- Set3/20/countLines.cc
+++ Set3/20/countLines.cc
@@ -5,7 +5,7 @@ size_t countLines()
     size_t lineCount = 0;
     
     string line;
-    while(getline(cin, line))
+    while (getline(cin, line))
         ++lineCount;
     
     return lineCount;
