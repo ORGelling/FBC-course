252921c6a6e6d1301a947485080925576219ff0f
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-22 00:31:54 +0200

    Update Set3/17/sum2.cc
    Co-authored-by: Leon Lan <30997278+leonlan@users.noreply.github.com>

--- Set3/17/sum2.cc
+++ Set3/17/sum2.cc
@@ -5,7 +5,7 @@ double sum(size_t const argc, char const *argv[], char const *pickDouble)
     double doubleSumValue = 0.0;
     for (size_t argIdx = 1; argIdx != argc; ++argIdx)
     {
-        double argument = stod(argv[argIdx]);
+        double const argument = stod(argv[argIdx]);
         doubleSumValue += argument;
     }
     return doubleSumValue;
