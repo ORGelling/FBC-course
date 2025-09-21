1b2568a966a790611b6286f645600ea2df84f8d1
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-22 00:31:44 +0200

    Update Set3/17/sum1.cc
    consts!
    
    Co-authored-by: Leon Lan <30997278+leonlan@users.noreply.github.com>

--- Set3/17/sum1.cc
+++ Set3/17/sum1.cc
@@ -5,7 +5,7 @@ int sum(size_t const argc, char const *argv[])
     int intSumValue = 0;
     for (size_t argIdx = 1; argIdx != argc; ++argIdx)
     {
-        int argument = stoi(argv[argIdx]);
+        int const argument = stoi(argv[argIdx]);
         intSumValue += argument;
     }
     return intSumValue;
