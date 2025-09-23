253c4b15731420614bee6427dc493afef946018d
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-23 15:30:33 +0200

    feedback 2

---
+++ Set2Feedback/set2-final-rated/13/test.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int value = 8;
+    
+    //if (true) if (false) value = 5; else value = 6;
+    
+    //if (true) 
+    //    if (false)                        // We can test which if gets us
+    //        value = 5;
+    //else                                  // to the else here
+    //    value = 6;
+
+    //if (true)                             // This one has no else now
+    //    if (false)                        // This leads to the else
+    //        value = 5; 
+    //    else 
+    //        value = 6;
+    
+    //if (false)                            // Now this one does
+    //{
+    //    if (false)                        // This one does not 
+    //        value = 5; 
+    //}
+    //else 
+    //    value = 6;
+    
+    if (true)
+        value = (true ? 5 : 6);
+    
+    cout << value << '\n';
+}
---
+++ Set2Feedback/set2-final-rated/16/exercise16.cc
@@ -0,0 +1,37 @@
+// HB: rating: 1
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace {
+    enum { 
+    // HB: L x2
+        UTF8_CONT_SEQ = 0b11000000,     // First two MSBs, or 192
+        // HB: sure, it's sort of the contiuation sequence, but you're only using it as mask later on
+
+        UTF8_END_SEQ = 0b10000000       // Only MSB, or 128
+    };
+}
+
+int main()
+{
+    string line;
+    while (getline(cin, line))                  // Read line from input
+    {                                           // case of file
+        size_t chIndex = line.size(); 
+        while(chIndex) // HB: BAK
+        {
+            size_t nrBytes = 1;
+            while ((line[chIndex - 1] & UTF8_CONT_SEQ) == UTF8_END_SEQ)  
+            {                       // Check if MSB is set but next is not
+                --chIndex;                      // Move to next byte
+                ++nrBytes;                      // Store extra byte #
+            }
+            cout << line.substr(chIndex - 1, nrBytes);
+            --chIndex;
+        }                       // Show characters without truncating bytes
+    cout << '\n';
+    }
+}
