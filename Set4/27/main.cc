0a960e22a8a180f52904c1a48d351d0c09db9096
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-26 19:13:23 +0200

    final touches ex 27

--- Set4/27/main.cc
+++ Set4/27/main.cc
@@ -6,9 +6,7 @@ int main(int argc, char *argv[])
     
     Person personArray[arraySize];
     
-    // call reader function
     readIntoPerson(personArray, arraySize);
     
-    // call writer function
     writeFromPerson(personArray, arraySize);
 }
