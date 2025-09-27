0bdfeaa548ed026d9ad17a1f2436e33e66b0ae58
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-27 13:46:15 +0200

    Update Set4/28/line/getLine.cc
    Simplifying getLine()
    
    Co-authored-by: Leon Lan <30997278+leonlan@users.noreply.github.com>

--- Set4/28/line/getLine.cc
+++ Set4/28/line/getLine.cc
@@ -6,8 +6,5 @@ bool Line::getLine()
 {
     getline(cin, d_line);
     d_position = d_line.find_first_not_of(" \t\n\r\f\v");
-    if (d_position != string::npos)
-        return true;
-    
-    return false;
+    return d_position != string::npos;
 }
