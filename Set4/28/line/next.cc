2bf15cdd11c6fed4d018d3e88570ab6bd8d053e3
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-27 13:48:04 +0200

    Update Set4/28/line/next.cc
    Co-authored-by: Leon Lan <30997278+leonlan@users.noreply.github.com>

--- Set4/28/line/next.cc
+++ Set4/28/line/next.cc
@@ -6,9 +6,8 @@ string Line::next()
         return "";
                                                         // Find next ws 
     size_t endPosition = d_line.find_first_of(" \t", d_position);
-    string subString = d_line.substr(d_position, endPosition - d_position);
-                                                        // Cut next substring
     d_position = d_line.find_first_not_of(" \t", endPosition);
                                         // set d_pos to next non ws position
-    return subString;
+    return d_line.substr(d_position, endPosition - d_position);
+                                                        // Cut next substring;
 }
