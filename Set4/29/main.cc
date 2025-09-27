78e8d8cc979ce490ca39eb3a0d119ddda04ea970
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-27 17:55:22 +0200

    testing main added and small improvements

---
+++ Set4/29/main.cc
@@ -0,0 +1,14 @@
+#include "parser/parser.h"
+#include "line/line.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    Parser parse;
+    
+    parse.reset();
+}
