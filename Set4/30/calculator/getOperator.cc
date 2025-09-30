a579fbbff90bdf086bcaf48ca9ca0d7c0920d6df
Olivier Gelling <o.r.gelling@hotmail.com>
2025-09-30 16:01:53 +0200

    Re-added operand error message

--- Set4/30/calculator/getOperator.cc
+++ Set4/30/calculator/getOperator.cc
@@ -7,7 +7,14 @@ bool Calculator::getOperator()
     string const substring = d_parser.next();
     
     d_op = substring.empty() ? '\0' : substring[0];
+    string const validOperators = "+-*/%"; 
     
-    return d_op == '+' || d_op == '-' || d_op == '*' || d_op == '/'
-           || d_op == '%';
+    if (validOperators.find(d_op) == string::npos)
+    {
+        cout << "invalid operator\n";
+        return false;
+    }
+    return true;
+    //return d_op == '+' || d_op == '-' || d_op == '*' || d_op == '/'
+    //       || d_op == '%';
 }
