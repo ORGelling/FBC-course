6166e4f5555f350f22c4653aaf3086855f335c4c
Olivier Gelling <o.r.gelling@hotmail.com>
2025-10-03 00:38:45 +0200

    ex 36 done

---
+++ Set5/36/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << ++argv[0][2] << '\n';
+    cout << argv[0][2] << '\n';
+    
+    cout << ++*(*argv + 2) << '\n';
+    cout << *argv << '\n';
+
+    //int x[8];
+    
+    //cout << &x[10] - &x[3] << '\n';
+}
