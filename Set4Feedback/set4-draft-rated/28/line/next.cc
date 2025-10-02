a0d5dcdd9bd206f3b3d544e0a1a4ba9423a20749
Olivier Gelling <o.r.gelling@hotmail.com>
2025-10-02 16:32:01 +0200

    got rid of ternary in 28 next()

--- Set4Feedback/set4-draft-rated/28/line/next.cc
+++ Set4Feedback/set4-draft-rated/28/line/next.cc
@@ -6,9 +6,10 @@ string Line::next()
         return "";
                                                         // Find next ws 
     size_t endPosition = d_line.find_first_of(" \t", d_position);
-    
-    string const subString = 
-                    d_line.substr(d_position, endPosition - d_position);
+    string subString =                                  // Cut next substring
+        (endPosition == string::npos)
+        ? d_line.substr(d_position)
+        : d_line.substr(d_position, endPosition - d_position);
     
     d_position = d_line.find_first_not_of(" \t", endPosition);
                                         // set d_pos to next non ws position
