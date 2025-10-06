79ec6484a72bd39a9efacd2f9c1ddb0f08cb89bf
Olivier Gelling <o.r.gelling@hotmail.com>
2025-10-06 11:58:30 +0200

    minor updates 35

--- Set5/35/main.cc
+++ Set5/35/main.cc
@@ -6,7 +6,7 @@ int main()
     counter.count(cin);
     
     CharCount::CharInfo infoData = counter.info();
-    size_t nChars = infoData.nCharObjects;
+    size_t const nChars = infoData.nCharObjects;
     
     for (size_t index = 0; index != nChars; ++index)
         showChar(infoData.ptr[index]);
