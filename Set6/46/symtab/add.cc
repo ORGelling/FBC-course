448287deb58efb2bfb79eedaac66680eb6f80be3
Olivier Gelling <o.r.gelling@hotmail.com>
2025-10-11 03:15:59 +0200

    code for 46 is done!

--- Set6/46/symtab/add.cc
+++ Set6/46/symtab/add.cc
@@ -4,11 +4,9 @@
 
 void Symtab::add(string const &ident, size_t const position)
 {
-    if (d_size == d_capacity)
+    if (d_size++ == d_capacity)
         enlarge(d_capacity * 2);
     
     movePtrs(position);
     d_data[position] = new Symbol{ ident };
-    
-    ++d_size;
 }
