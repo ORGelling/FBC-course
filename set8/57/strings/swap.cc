1f4874c0ad4f7b5d6dd2732d273fd66f7541fe9d
Olivier Gelling <o.r.gelling@hotmail.com>
2025-10-26 21:11:08 +0100

    Update set8/57/strings/swap.cc
    Co-authored-by: Leon Lan <30997278+leonlan@users.noreply.github.com>

--- set8/57/strings/swap.cc
+++ set8/57/strings/swap.cc
@@ -2,15 +2,7 @@
 
 void Strings::swap(Strings &other)
 {
-    string **tmp = d_data;
-    d_data = other.d_data;
-    other.d_data = tmp;
-
-    size_t size = d_size;
-    d_size = other.d_size;
-    other.d_size = size;
-
-    size_t tmpCapacity = d_capacity;
-    d_capacity = other.d_capacity;
-    other.d_capacity = tmpCapacity;
+    std::swap(d_data, other.d_data);
+    std::swap(d_size, other.d_size);
+    std::swap(d_capacity, other.d_capacity);
 }
