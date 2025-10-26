d24d93fbc257adf1c0ca164a4eb946bff58f80bd
Olivier Gelling <o.r.gelling@hotmail.com>
2025-10-26 23:43:47 +0100

    63 code done. Most of info file too

--- set8/63/wrap/operatorassign1.cc
+++ set8/63/wrap/operatorassign1.cc
@@ -4,6 +4,8 @@
 
 Wrap &Wrap::operator=(Wrap const &other)
 {
+//  d_data.copyAssign(d_type, other.d_data, other.d_type);
+//  d_type = other.d_type;
     Wrap tmp{ other };
     swap(tmp);
     return *this;
