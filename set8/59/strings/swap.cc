2a938e037b84fc7d8822aa556cb98f73ec2bbc3d
Olivier Gelling <o.r.gelling@hotmail.com>
2025-10-27 15:11:18 +0100

    improved swapping using std::swap

--- set8/59/strings/swap.cc
+++ set8/59/strings/swap.cc
@@ -2,11 +2,6 @@
 
 void Strings::swap(Strings &other)
 {
-    string *tmp = d_str;
-    d_str = other.d_str;
-    other.d_str = tmp;
-
-    size_t size = d_size;
-    d_size = other.d_size;
-    other.d_size = size;
+    std::swap(d_str, other.d_str);
+    std::swap(d_size, other.d_size);
 }
