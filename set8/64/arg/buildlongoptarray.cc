59dda4b81602b75a05f3fe17cf7e16d4541b2523
Olivier Gelling <o.r.gelling@hotmail.com>
2025-10-29 15:38:40 +0100

    debugging has further improved the program. Long options are now being properly stored and recognised. Values can be accessed using size(value, ntbs)

--- set8/64/arg/buildlongoptarray.cc
+++ set8/64/arg/buildlongoptarray.cc
@@ -14,12 +14,11 @@ void Arg::buildLongOptArray
     {
         options[index].name = const_cast<char *>(begin[index].name().c_str());
         options[index].has_arg = 
-                            (begin[index].type() == Arg::Required) ? 
-                            required_argument : 
-                                (begin[index].type() == Arg::Optional) ? 
-                                optional_argument : no_argument;
+                (begin[index].type() == Arg::Required) ? required_argument : 
+                (begin[index].type() == Arg::Optional) ? 
+                                        optional_argument : no_argument;
         options[index].flag = nullptr;
         options[index].val = begin[index].optionChar() ? 
-            begin[index].optionChar() : 0;
+                begin[index].optionChar() : 0;
     }
 }
