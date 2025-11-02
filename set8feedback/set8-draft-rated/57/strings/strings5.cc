df12459680fda592a1921a5deef6b15e7713a376
Olivier Gelling <o.r.gelling@hotmail.com>
2025-11-02 16:54:59 +0100

    fixing line length from feedback

--- set8feedback/set8-draft-rated/57/strings/strings5.cc
+++ set8feedback/set8-draft-rated/57/strings/strings5.cc
@@ -5,10 +5,12 @@
 Strings::Strings(Strings const &other)
 :
     d_size(other.d_size),
-    d_capacity(other.d_capacity), // HB: we insist you copy from other.d_size, since that fits and saves memory
+    d_capacity(other.d_capacity), // HB: we insist you copy from other.d_size,
+                                  // HB: since that fits and saves memory
     d_data(rawPointers(other.d_capacity))
 {
     // Use temp and then swap?
-    // HB: not necessary: you'd do eaxctly what you'd do here anyway, so you're just being inefficient
+    // HB: not necessary: you'd do eaxctly what you'd do here anyway, 
+    // HB: so you're just being inefficient
     copyStringsFrom(other.d_data);
 }
