c29283ba6ca60bb445fc6f58f0dd22890a2d05e1
Olivier Gelling <o.r.gelling@hotmail.com>
2025-11-17 20:44:58 +0100

    comments and changes. 1 and 2 could use some minor revisions wrt overzealous use of friend keyword

--- part2/set1/1/numbers/opequal.cc
+++ part2/set1/1/numbers/opequal.cc
@@ -6,4 +6,4 @@ bool operator==(Numbers const &lhs, Numbers const &rhs)
 {
     return Numbers::compareSize(lhs, rhs) 
                                 and Numbers::compareContents(lhs, rhs);
-}                                       // scope selected by Koenig lookup
+}
