db8dbf76c92dca7b0f3dcf1c5fe35dad5878cfc7
Olivier Gelling <o.r.gelling@hotmail.com>
2025-11-19 12:56:24 +0100

    more testing of 8

--- part2/set1/8/main.cc
+++ part2/set1/8/main.cc
@@ -5,11 +5,15 @@ int main(int argc, char **argv)
     Strings one(argc, argv);
     Strings two(cin);
     
+    two += "Last line";
     Strings thr(two);
     
     string newStr("New entry!");
     thr += newStr;
     
+    thr[1] = "inserted line";
+    
     for (size_t index = 0; index != thr.size(); ++index)
         cout << thr[index] << '\n';
+
 }
