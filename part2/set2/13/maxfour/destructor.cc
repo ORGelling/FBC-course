f609527a99fa06c726d9336cf95ba26c3fecb8a1
Olivier Gelling <o.r.gelling@hotmail.com>
2025-11-22 19:21:50 +0100

    maxfour class set up. Not done.

---
+++ part2/set2/13/maxfour/destructor.cc
@@ -0,0 +1,8 @@
+#include "maxfour.ih"
+
+    // by 
+
+MaxFour::~MaxFour()
+{
+    cout << "Calling destructor on: " << d_number << '\n';
+}
---
+++ part2/set2/13/maxfour/maxfour.h
@@ -0,0 +1,17 @@
+#ifndef INCLUDED_MAXFOUR_
+#define INCLUDED_MAXFOUR_
+
+#include <iosfwd>
+
+class MaxFour
+{
+    inline static size_t s_count;
+    
+    size_t d_number;
+    
+    public:
+        MaxFour();
+        ~MaxFour();
+};
+
+#endif
