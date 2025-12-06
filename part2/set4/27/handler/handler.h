833a73483d0c4730239f8d0ed2116a7165f44bc0
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-06 17:18:55 +0100

    ex 27 + info and order files

---
+++ part2/set4/27/handler/handler.h
@@ -0,0 +1,18 @@
+#ifndef INCLUDED_HANDLER_
+#define INCLUDED_HANDLER_
+
+#include "../msgenum/msgenum.h"
+
+class Handler: private MsgEnum
+{
+    public:
+        Handler() = default;
+        ~Handler() = default;
+
+        // member functions
+        
+    private:
+        // helpers etc
+};
+        
+#endif
