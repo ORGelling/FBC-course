c835f5930117256adaf41675ee6217662ca24618
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-07 18:00:12 +0100

    fixing 20 a bit

--- part2/set3/20/strings/repeatString.cc
+++ part2/set3/20/strings/repeatString.cc
@@ -4,7 +4,8 @@
 
 void Strings::repeatString(size_t count, string const &str)
 {
+    reserve(size() + count * str.size());           // avoids repeat allocs?
     for (size_t index = 0; index != count; ++index)
-        this->append(str);
+        append(str);
         //*this += str;
 }
