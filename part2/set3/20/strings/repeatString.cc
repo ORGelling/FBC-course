f2d3fa7b73c763b31d735dc7c594322066e0a5c5
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-09 18:52:01 +0100

    fixed 20 and 22

--- part2/set3/20/strings/repeatString.cc
+++ part2/set3/20/strings/repeatString.cc
@@ -7,5 +7,4 @@ void Strings::repeatString(size_t count, string const &str)
     reserve(size() + count * str.size());           // avoids repeat allocs?
     for (size_t index = 0; index != count; ++index)
         append(str);
-        //*this += str;
 }
