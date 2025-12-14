31bd41bad1e24a0d096f43f9a4849ee27fb93436
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-14 02:11:53 +0100

    final amendments after second feedback

--- part2/set3/20/strings/strings.h
+++ part2/set3/20/strings/strings.h
@@ -9,19 +9,7 @@ class Strings: public std::string
         using std::string::string;
         using std::string::operator=;
         
-        Strings() = default;
-        Strings(Strings const &other) = default;
-        Strings(Strings &&tmp) = default;
-        
         Strings(size_t count, std::string const &str);
-        
-        ~Strings() = default;
-        
-        Strings &operator=(Strings const &other) = default;
-        Strings &operator=(Strings &&other) = default;
-        
-    private:
-        void repeatString(size_t count, std::string const &str);
 };
         
 #endif
