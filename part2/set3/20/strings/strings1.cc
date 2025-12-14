d555e06dd7ee4628f555b87f0171637794a4e9ed
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-14 02:11:53 +0100

    final amendments after second feedback

--- part2/set3/20/strings/strings1.cc
+++ part2/set3/20/strings/strings1.cc
@@ -6,5 +6,7 @@ Strings::Strings(size_t count, string const &str)
 //:                     // explicitly constructing empty string unnecessary
 //    string()          // empty string is default constructed
 {
-    repeatString(count, str);
+    reserve(size() + count * str.size());       // avoids repeat allocs
+    for (size_t index = 0; index != count; ++index)
+        append(str);
 }
