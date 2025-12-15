7ee8543060559c1b8dd556966be505edc5f397cc
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-16 00:12:23 +0100

    ex 38 mostly done

--- part2/set5/38/address/address.h
+++ part2/set5/38/address/address.h
@@ -1,13 +1,28 @@
 #ifndef INCLUDED_ADDRESS_
 #define INCLUDED_ADDRESS_
+#include "../postalhash/postalhash.h"     // has PostalInfo and PostalHash
+#include <unordered_map>
+#include <vector>
+#include <iosfwd>
 
+using PostalInfo = std::pair<std::string, std::string>;
+using Occupants = std::vector<std::string>;    
 
 class Address
 {
+    friend std::istream &operator>>(std::istream &in, Address &address);
+    friend std::ostream &operator<<(std::ostream &out, Address &address);
+    
+    std::unordered_map<PostalInfo, Occupants, PostalHash> d_umap;
+    
     public:
-        Address();
+        size_t remove(std::string const &postCode);
+        // postal code, should work w/ initial part to remove multiple areas
+        // returns remaining nr of elements
 
     private:
+        std::ostream &insertInto(std::ostream &dest);
+        std::istream &extractFrom(std::istream &source);
 };
         
 #endif
---
+++ part2/set5/38/address/insertinto.cc
@@ -0,0 +1,16 @@
+#include "address.ih"
+
+    // by 
+
+ostream &Address::insertInto(ostream &out)
+{ 
+    for (auto const &value: d_umap)             // d_umap is the map 
+    { 
+        out << value.first.first << "; "        // postal code 
+            << value.first.second;              // address
+        
+        for (string const &str: value.second)   // the people (maybe 
+            out << "; " << str;                 // define a function?)
+    }
+    return out << '\n';
+} 
