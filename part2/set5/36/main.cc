277e707bfb4ccce32eefd551cbb0caa758b2c245
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-20 23:30:23 +0100

    comment and final changes. Order and info files not done

--- part2/set5/36/main.cc
+++ part2/set5/36/main.cc
@@ -8,8 +8,12 @@ int main()
     while (getline(cin, str))
         stringVector += str;
     
+    //cerr << "Object constructed. Operations follow:\n\n";
+    
     stringVector[2] = "Inserting line using Proxy";
     stringVector += stringVector[2];
+    
+    //cerr << "\nOperations done. Printing follows:\n\n";
     
     for (size_t index = 0; index != stringVector.size(); ++index)
         cout << stringVector.at(index) << '\n';
--- part2/set5/36/strings/at1.cc
+++ part2/set5/36/strings/at1.cc
@@ -3,7 +3,7 @@
     // by 
 
 Strings::Proxy Strings::at(size_t idx)
-{
+{// this check is kinda weird and unclear
     static_cast<void>(d_data.at(idx));          // test oob with vector at()
     return Proxy(*this, idx);                   // rest can simply use op[]
 }
