b5457b61de7c44b780db7c5442b5824c2b9de7a9
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-20 21:11:23 +0100

    implementing COW

--- part2/set5/36/strings/at1.cc
+++ part2/set5/36/strings/at1.cc
@@ -4,8 +4,6 @@
 
 string &Strings::at(size_t idx)
 {
-    if (!d_data.at(idx).unique())              // NOT proper COW
-        d_data[idx] = make_shared<string>(*d_data[idx]);
-                                                // first at checks bounds,
-    return *d_data[idx];                        // rest can simply use op[]
+    d_data.at(idx);                             // test oob with vector at()
+    return Proxy(*this, idx);                   // rest can simply use op[]
 }
