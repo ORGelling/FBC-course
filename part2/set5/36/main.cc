ccebfad63034f5088f0465c00e91def4ba4d9f43
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-21 05:03:26 +0100

    final testing and changes done. ORDER NOT ADDED

--- part2/set5/36/main.cc
+++ part2/set5/36/main.cc
@@ -4,7 +4,7 @@ int main()
 {
     Strings stringVector;
     string str;
-  //while (cin >> str)
+
     while (getline(cin, str))
         stringVector += str;
     
@@ -12,9 +12,13 @@ int main()
     
     stringVector[2] = "Inserting line using Proxy";
     stringVector += stringVector[2];
+    stringVector[4] = stringVector[0];
     
     //cerr << "\nOperations done. Printing follows:\n\n";
     
     for (size_t index = 0; index != stringVector.size(); ++index)
         cout << stringVector.at(index) << '\n';
+    
+    //cout << stringVector[100];        // returns empty from safeAt
+    //cout << stringVector.at(100);
 }
