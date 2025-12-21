4a9c40421aefe374ac86e1c2904532cb85680678
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-21 22:16:43 +0100

    improved other two exercises

--- part2/set5/38/address/address.h
+++ part2/set5/38/address/address.h
@@ -5,20 +5,16 @@
 #include <vector>
 #include <iosfwd>
 
-using PostalInfo = std::pair<std::string, std::string>; // this does pollute
-using Occupants = std::vector<std::string>;             // global namespace
-
-// Improve namings. Add 3rd using clause? Reconsider format.
-// Consider changing Address. Extra subclass for occupants?
+                                   // Postal code // address
+using PostCodeAndAddress = std::pair<std::string, std::string>;
+using Occupants = std::vector<std::string>;
 
 class Address
 {
     friend std::istream &operator>>(std::istream &in, Address &address);
     friend std::ostream &operator<<(std::ostream &out, Address &address);
     
-    // Can declare an extra public section up here to place the using decls?
-    
-    std::unordered_map<PostalInfo, Occupants, PostalHash> d_umap;
+    std::unordered_map<PostCodeAndAddress, Occupants, PostalHash> d_umap;
     
     public:
         size_t remove(std::string const &postCode);
