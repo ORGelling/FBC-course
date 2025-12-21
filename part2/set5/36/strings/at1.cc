5fd118204171794a8c5c40e48a703d2b5f543927
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-21 05:03:26 +0100

    final testing and changes done. ORDER NOT ADDED

--- part2/set5/36/strings/at1.cc
+++ part2/set5/36/strings/at1.cc
@@ -3,7 +3,10 @@
     // by 
 
 Strings::Proxy Strings::at(size_t idx)
-{// this check is kinda weird and unclear
-    static_cast<void>(d_data.at(idx));          // test oob with vector at()
+{
+    if (idx >= d_data.size())
+        throw out_of_range{ "Index is out of range" };
+    // this check is kinda weird and unclear
+    // static_cast<void>(d_data.at(idx));       // test oob with vctr's at()
     return Proxy(*this, idx);                   // rest can simply use op[]
 }
