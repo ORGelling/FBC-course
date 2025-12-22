f7bbd304e585083dd32592f8a805ce7d38cb9be3
Olivier Gelling <o.r.gelling@hotmail.com>
2025-12-22 22:39:12 +0100

    working through extra feedback from Jurjen

--- part2/set5/36/strings/proxyconvertstring.cc
+++ part2/set5/36/strings/proxyconvertstring.cc
@@ -4,5 +4,8 @@
 
 Strings::Proxy::operator string const &() const
 {
-    return p_owner->safeAt(p_idx);
+    cerr << "convertString index " << p_idx << '\n';
+  //return p_owner->safeAt(p_idx);
+  //return *p_owner->d_data.at(p_idx);
+    return *p_owner->d_data[p_idx];
 }
--- part2/set5/36/strings/proxyopass3.cc
+++ part2/set5/36/strings/proxyopass3.cc
@@ -4,6 +4,7 @@
 
 Strings::Proxy &Strings::Proxy::operator=(Proxy const &rhs)
 {
+    cerr << "opassign3\n";
     string tmp = rhs;                           // use conversion op
     
     return *this = tmp;                         // delegates to other op=
