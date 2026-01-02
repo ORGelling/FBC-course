ffe47fc8d3519946e60269e03270adec0bbae6f0
Olivier Gelling <o.r.gelling@hotmail.com>
2026-01-02 20:20:23 +0100

    read function is done too

--- part2/set6/44/main.cc
+++ part2/set6/44/main.cc
@@ -5,12 +5,7 @@ int main(int argc, char **argv)
     if (argc != 2)
         return 1;
     
-    ifstream file(argv[1]);
-    vector<Student> studVec;
-    
-    Student student;
-    while (file >> student)
-        studVec.push_back(student);
+    vector<Student> studVec = read(argv[1]);
     
     for(auto element : studVec)
         cout << element;
