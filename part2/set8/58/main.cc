25d38c24915734cf7b32c728d0b06eeaa5bc8f82
Olivier Gelling <o.r.gelling@hotmail.com>
2026-01-18 01:44:10 +0100

    58 done

--- part2/set8/58/main.cc
+++ part2/set8/58/main.cc
@@ -1,19 +1,68 @@
 #include "main.ih"
 
+
+namespace {
+    
+    
+    double innerProduct(double const *lhs, double const *rhs, size_t size)
+    {
+        return inner_product(lhs, lhs + size, rhs, 0.0);
+    }                           // wrapper since inner_product is a template
+    
+    
+    void showMatrix(double const (*matrix)[6], size_t const lhSize, 
+                                                        size_t const rhSize)
+    {
+        for (size_t idxI = 0; idxI != lhSize; ++idxI)
+        {
+            for (size_t idxJ = 0; idxJ != rhSize; ++idxJ)
+                cout << matrix[idxI][idxJ] << ' ';
+            cout << '\n';           
+        }                                   // simple output refactor
+    }
+    
+    
+    void multiplyMatrices(
+        double const (*lhs)[5], 
+        double const (*rhs)[5],
+        future<double> fut[][6]
+    )
+    {
+        for (size_t idxI = 0; idxI != 4; ++idxI)
+        {
+            for (size_t idxJ = 0; idxJ != 6; ++idxJ)
+            {
+                Task task{ innerProduct };
+                fut[idxI][idxJ] = task.get_future();
+                thread(move(task), &lhs[idxI][0], &rhs[idxJ][0], 5).detach();
+            }
+        }
+    }
+    
+    
+}   // namespace
+
+
 int main()
 {
-    // fill matrices with random values
     double lhs[4][5];
-    double rhs[6][5];       // already transposed
+    double rhs[6][5];                           // already transposed
     
-    iota(&lhs[0][0], &lhs[3][4] + 1, 3.14);
-    iota(&rhs[0][0], &rhs[5][4] + 1, 4.2);
+    future<double> fut[4][6];                   // handling results
+    double results[4][6];
     
-//  copy(&lhs[0][0], &lhs[3][4] + 1, ostream_iterator<double>(cout, " "));
-//  cout << '\n';
-//  copy(&rhs[0][0], &rhs[5][4] + 1, ostream_iterator<double>(cout, " "));
-//  cout << '\n';
+    iota(&lhs[0][0], &lhs[3][4] + 1, 3.14);     // fill with values using
+    iota(&rhs[0][0], &rhs[5][4] + 1, 4.2);      // sequential memory layout
     
-    // take inner product of the rows.
     
+    multiplyMatrices(lhs, rhs, fut);
+    
+    
+    for (size_t idxI = 0; idxI != 4; ++idxI)    // collect futures
+    {
+        for (size_t idxJ = 0; idxJ != 6; ++idxJ)
+            results[idxI][idxJ] = fut[idxI][idxJ].get();
+    }                               // considering refactor, but seems ok?
+    
+    showMatrix(results, 4, 6);
 }
