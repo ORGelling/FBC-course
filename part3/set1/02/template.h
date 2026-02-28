7f44aaef5733707998408b326c3af58ae4c20e54
ORGelling <o.r.gelling@hotmail.com>
2026-02-28 18:49:23 +0100

    test

--- part3/set1/02/template.h
+++ part3/set1/02/template.h
@@ -1,10 +1,12 @@
 #ifndef INCLUDED_TEMPLATE02_
 #define INCLUDED_TEMPLATE02_
 
+#include <utility>      // for forward
+
 template <typename To, typename From>
 To as(From &&from)
 {
-    return static_cast<To>(from);
+    return static_cast<To>(std::forward<From>(from));
 }
 
 #endif
