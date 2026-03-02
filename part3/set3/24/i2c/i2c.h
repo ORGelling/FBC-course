66842428ff67185782fecf2bbea378f903685f7e
ORGelling <o.r.gelling@hotmail.com>
2026-03-02 23:31:24 +0100

    ex 24

--- part3/set3/24/i2c/i2c.h
+++ part3/set3/24/i2c/i2c.h
@@ -6,31 +6,38 @@
 template <size_t Integer>
 class I2C
 {
-    template <char ...Chars>
-    struct Digits
-    {
+    template <char ...Chars>    //    3:    // at the end of the recursion we
+    struct Digits                           // inherit this to turn our char 
+    {                                       // parameter pack into an ntbs
         static constexpr char const s_ntbs[] = 
         {
             Chars...,
             0
         };
     };
-    
+                                //    2:    // We self-inherit to bite
+                                            // off a digit each step
     template <size_t Remaining, char ...Chars>
     struct Build : Build<Remaining / 10, '0' + (Remaining % 10), Chars...>
+    {};             //   ^^^^^^^^^^^^^^  ^^^^^^^^^^^^^^^^^^^^^^  ^^^^^^^^
+                    //    removes the    //  returns only the  //  converted
+                    //  rightmost digit  //   rightmost digit  //  characters
+    
+                                // this takes the rightmost digit of Integer,
+                                // converts it to a char using '0' + value,
+                                // *pre*pends it to Chars parameter pack,
+                                // which recursively inherits the other digits
+    
+    template <char ...Chars>
+    struct Build<0, Chars...> : Digits<Chars...>
     {};
     
-
-    template <size_t Remaining, char ...Chars>
-    struch Build<0, Chars...> : Digits<Chars...>
-    {};
-    
-    public:
-        static constexpr char const s_ntbs[] = Build<Integer>::s_ntbs;
-};
+    public:                     //    1:    // starts inheritance chain
+        static constexpr char const *s_ntbs = Build<Integer>::s_ntbs;
+};                              //  ^  pointer since size unknown initially
 
 template <>
-class I2C<0>
+class I2C<0>                    // Overload for 0-case, which fails otherwise
 {
     public:
         static constexpr char s_ntbs[] = 
