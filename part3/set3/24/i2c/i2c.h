2517a76b15cd0c90f7422ee0ff6f9398f167dd6f
ORGelling <o.r.gelling@hotmail.com>
2026-03-07 15:41:46 +0100

    cleanup and renaming

--- part3/set3/24/i2c/i2c.h
+++ part3/set3/24/i2c/i2c.h
@@ -3,41 +3,30 @@
 
 #include <cstddef>
 
-template <size_t Integer>
-class I2C
-{
-    template <char ...Chars>    //    3:    // at the end of the recursion we
-    struct Digits                           // inherit this to turn our char 
-    {                                       // parameter pack into an ntbs
-        static constexpr char const s_ntbs[] = 
-        {
-            Chars...,
-            0
-        };
+
+template <char ...Chars>    //    3:    // at the end of the recursion we
+struct Digits                           // inherit this to turn our char 
+{                                       // parameter pack into an ntbs
+    static constexpr char const s_ntbs[] = 
+    {
+        Chars...,
+        0
     };
-                                //    2:    // We self-inherit to bite
-                                            // off a digit each step
-    template <size_t Remaining, char ...Chars>
-    struct Build : Build<Remaining / 10, '0' + (Remaining % 10), Chars...>
-    {};             //   ^^^^^^^^^^^^^^  ^^^^^^^^^^^^^^^^^^^^^^  ^^^^^^^^
-                    //    removes the    //  returns only the  //  converted
-                    //  rightmost digit  //   rightmost digit  //  characters
-    
-                                // this takes the rightmost digit of Integer,
-                                // converts it to a char using '0' + value,
-                                // *pre*pends it to Chars parameter pack,
-                                // which recursively inherits the other digits
-    
-    template <char ...Chars>
-    struct Build<0, Chars...> : Digits<Chars...>
-    {};
-    
-    public:                     //    1:    // starts inheritance chain
-        static constexpr char const *s_ntbs = Build<Integer>::s_ntbs;
-};                              //  ^  pointer since size unknown initially
+};
+
+
+template <size_t Integer, char ...chars>
+struct I2C : I2C<Integer / 10, '0' + (Integer % 10), chars...>
+{};                         //    1:    // Inheriting recursively to pass a
+                                        // digit to the param pack each turn
+
+template <char ...chars>
+struct I2C<0, chars...> : Digits<chars...>
+{};                         //    2:    // When no digits left we inherit the
+                                        // conversion struct and get our ntbs
 
 template <>
-class I2C<0>                    // Overload for 0-case, which fails otherwise
+struct I2C<0>                    // Overload for 0-case, which fails otherwise
 {
     public:
         static constexpr char s_ntbs[] = 
