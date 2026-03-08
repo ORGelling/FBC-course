6e8027d681b87fba465375a574de4de440908c5e
ORGelling <o.r.gelling@hotmail.com>
2026-03-08 22:40:30 +0100

    27

--- part3/set4/27/data/data.h
+++ part3/set4/27/data/data.h
@@ -1,27 +1,20 @@
 #ifndef INCLUDED_DATA_
 #define INCLUDED_DATA_
 
-// 
+#include "../datatraits/datatraits.h" 
 
-#include <string>
 #include <vector>
-#include <cstddef>
-
-// 
-
-// struct for string
-// struct for built-in
-// struct for floating
 
 class Data
 {
     std::vector<std::string> d_data;
     
     public:
+        Data() = default;
         Data(std::initializer_list<std::string> const &list);
         
         template <typename Type>
-        decltype(auto) get<Type>(size_t idx) const;
+        decltype(auto) get(size_t idx) const;
 };
 
 #include "get.i"
--- part3/set4/27/datatraits/datatraits.h
+++ part3/set4/27/datatraits/datatraits.h
@@ -1,12 +1,108 @@
 #ifndef INCLUDED_DATATRAITS_
 #define INCLUDED_DATATRAITS_
 
+#include <string>
+#include <cstddef>
+
+struct StringTag
+{};
+
+struct IntegerTag
+{};
+
+struct DecimalTag
+{};
 
 template <typename Type>
-struct DataTraits
+struct Traits;
+
+template <>
+struct Traits<std::string>
 {
-    enum {
-        isString = true
+    using Tag = StringTag;
 };
-        
+
+// Using macros to define all other specialisations of this struct
+
+#define INTEGER_TYPE(Type)      \
+    template <>                 \
+    struct Traits<Type>         \
+    {                           \
+        using Tag = IntegerTag; \
+    };
+
+INTEGER_TYPE(bool);
+INTEGER_TYPE(short);
+INTEGER_TYPE(unsigned short);
+INTEGER_TYPE(int);
+INTEGER_TYPE(unsigned int);
+INTEGER_TYPE(long);
+INTEGER_TYPE(unsigned long);
+INTEGER_TYPE(long long);
+INTEGER_TYPE(unsigned long long);
+INTEGER_TYPE(char);
+INTEGER_TYPE(signed char);
+INTEGER_TYPE(unsigned char);
+
+#undef INTEGER_TYPE
+
+#define DECIMAL_TYPE(Type)      \
+    template <>                 \
+    struct Traits<Type>         \
+    {                           \
+        using Tag = DecimalTag; \
+    };
+
+DECIMAL_TYPE(float);
+DECIMAL_TYPE(double);
+DECIMAL_TYPE(long double);
+
+#undef DECIMAL_TYPE
+
+template <typename Type, typename Tag>
+struct Convert;
+
+
+template <typename Type>
+struct Convert<Type, StringTag>
+{
+    static std::string const &convert(std::string const &text)
+    {
+        return text;
+    }
+};
+
+template <typename Type>
+struct Convert<Type, IntegerTag>
+{
+    static Type convert(std::string const &text)
+    {
+        try
+        {
+            return static_cast<Type>(std::stoll(text));
+        }
+        catch (...)
+        {
+            return Type{};
+        }
+    }
+};
+
+template <typename Type>
+struct Convert<Type, DecimalTag>
+{
+    static Type convert(std::string const &text)
+    {
+        try
+        {
+            return static_cast<Type>(std::stold(text));
+        }
+        catch (...)
+        {
+            return Type{};
+        }
+    }
+};
+
+
 #endif
