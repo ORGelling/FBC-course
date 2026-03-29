6d011352fc0f99a965914eb0248f938eafcfdb84
ORGelling <o.r.gelling@hotmail.com>
2026-03-29 20:10:07 +0200

    set 6 is done

--- part3/set6/44/scanner/setshift.cc
+++ part3/set6/44/scanner/setshift.cc
@@ -2,6 +2,6 @@
 
 void Scanner::setShift()
 {
-    size_t newLineNr = stoul(matched().substr(2));
+    size_t newLineNr = stoul(&(matched().front()) + 2);
     d_shift = newLineNr - ScannerBase::lineNr();
 }
