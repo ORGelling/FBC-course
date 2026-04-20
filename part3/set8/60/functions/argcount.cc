c9cdb982edae860eb736593159a227a629225edd
ORGelling <o.r.gelling@hotmail.com>
2026-04-20 23:21:41 +0200

    amended order file

--- part3/set8/60/functions/argcount.cc
+++ part3/set8/60/functions/argcount.cc
@@ -5,6 +5,6 @@ void Functions::argCount(ArgVec const &args, size_t size) const
     if (args.size() != size)
     {
         throw runtime_error{ "incorrect nr of arguments: expected " +
-        to_string(size) + " got " + to_string(args.size()) };
+                    to_string(size) + " got " + to_string(args.size()) };
     }
 }
